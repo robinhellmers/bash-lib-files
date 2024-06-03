aebd87ae5a412ad7072df57359b235fbe5e65963
Robin Hellmers <robin@hellmers.se>
2024-06-03 16:28:27 +0200

    Add is_windows_path() & is_linux_path()

--- lib_files.bash
+++ lib_files.bash
@@ -40,3 +40,104 @@ init_lib
 #####################
 ### Library start ###
 #####################
+
+###
+# List of functions for usage outside of lib
+#
+# - is_windows_path()
+# - is_linux_path()
+###
+
+register_help_text 'is_windows_path' \
+"is_windows_path <path>
+
+Arguments:
+<path>:
+    Path to check
+"
+
+register_function_flags 'is_windows_path'
+
+is_windows_path()
+{
+    _handle_args 'is_windows_path' "$@"
+
+    local path="$1"
+    # Check if path contains backslashes (typically Windows)
+    [[ "$path" =~ \\ ]]
+}
+
+register_help_text 'is_linux_path' \
+"is_linux_path <path>
+
+Checks if path is a Linux path based on the strictness given.
+
+Arguments:
+<path>:
+    Path to check
+
+Strictness:
+'loose':
+    Only checks if there exists forward slashes
+'strict':
+    Actually checks if there is as path
+"
+
+register_function_flags 'is_linux_path' \
+                        '-s' '--strictness' 'true' \
+                        "Strictness of checking: 'loose' or 'strict' (default)"
+
+is_linux_path()
+{
+    local path strictness
+    _handle_args_is_linux_path "$@"
+
+    if [[ "$strictness" == 'loose' ]]
+    then
+        # Only check if there exists forward slashes
+        [[ "$path" =~ / ]]
+        return
+    fi
+
+    if [[ -f "$path" ]]
+    then
+        echo 1
+        [[ -d "$(dirname "$path")" ]]
+        return
+    fi
+
+    [[ -d "$path" ]]
+    return
+}
+
+
+_handle_args_is_linux_path()
+{
+    _handle_args 'is_linux_path' "$@"
+
+    ###
+    # Non-flagged arguments
+    path="${non_flagged_args[0]}"
+    ###
+
+    ###
+    # -s, --strictness
+    if [[ "$strictness_flag" == 'true' ]]
+    then
+        strictness="$strictness_flag_value"
+
+        case "$strictness" in
+            'loose')
+                ;;
+            'strict')
+                ;;
+            '')
+                strictness='strict'
+                ;;
+            *)
+                echo_error "Given strictness for is_linux_path() unknown: $strictness"
+                ;;
+        esac
+    fi
+    ###
+}
